0df9a60bb5fe91089d7dab9c294c90550de36d82
masset151 <masset.ruiz151@gmail.com>
2021-04-12 11:34:19 +0200

    esqueleto frontend

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,16 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
 
+
+export interface Cliente {
+  nombre:String,
+  genero:String
+}
+
 @Component({
   selector: 'app-no-comunes',
   templateUrl: './no-comunes.component.html',
   styles: [
   ]
 })
+
+
+
 export class NoComunesComponent {
+ 
   //i18Select
-  nombre: string = 'Andres'
-  genero: string = 'masculino'
+  nombre: string = 'Andres';
+  genero: string = 'masculino';
+  check:boolean = false;
+ 
+    
+ 
+
+ 
 
   invitacionMap = {
     'masculino':'invitarlo',
@@ -28,8 +44,53 @@ export class NoComunesComponent {
     '=2':'tenemos 2 clientes esperando',
     'other':'tenemos # clientes esperando'
     
+  
   }
 
- 
+  cambiarCliente(){
+
+    if(!this.check){
+      this.nombre="Ana"
+      this.genero = "femenino"
+
+      this.check = true;
+    }else{
+      this.nombre="Andres"
+      this.genero = "masculino"
+
+      this.check = false;
+    }
+    
+
+  }
+
+  borrarCliente(){
+    this.clientes.pop()
+  }
+
+ //keyValuePipe
+
+ persona = {
+   nombre:'Andres',
+   edad:'22',
+   direccion:'Sevilla, Andalucia'
+ }
+
+ //JsonPipe
+
+ heroes = [
+   {
+     nombre:"SuperMan",
+     vuelva:true
+   },
+   {
+    nombre:"Robin",
+    vuela:false
+  },
+  {
+    nombre:"Aquaman",
+    vuela:false
+  },
+  ]
 
 }
